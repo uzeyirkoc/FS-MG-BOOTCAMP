e01c44f86435349c50ebb7f4c20012229776773e
uzeyirkoc <110414822+uzeyirkoc@users.noreply.github.com>
2022-08-09 23:07:01 +0300

    Update main.go

--- backend/backend-kodlama-sorulari/golangSoru3/main.go
+++ backend/backend-kodlama-sorulari/golangSoru3/main.go
@@ -1,9 +1,18 @@
-package golangSoru3
+//package golangSoru3
 
 // Soru 4: IkiUstuN'i Geliştiriniz.
 
-func IkiUstuN(n int) int {
+//func IkiUstuN(n int) int {
 	// 0, 1, 2, 4, ... , 2^n-1 toplamlarını gösterecek bir yazılımı yazınız.
 	// Ipucu: Olabildiğince hızlı çalışması önemlidir.
-	return -1
+//	return -1
+//}
+package mgbootcamp
+
+func IkiUstun(n ...int) int {
+	toplam := 0
+	for i := 0; i < len(n); i++ {
+		toplam = toplam + 2 ^ i - 1
+	}
+	return toplam
 }
