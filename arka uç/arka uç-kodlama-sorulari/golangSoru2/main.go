c7c60f8161610209f6b229a3d8a23efe7e255e79
uzeyirkoc <110414822+uzeyirkoc@users.noreply.github.com>
2022-08-09 22:33:56 +0300

    Update and rename backend/backend-kodlama-sorulari/golangSoru2/main.go to arka uç/arka uç-kodlama-sorulari/golangSoru2/main.go

--- arka uç/arka uç-kodlama-sorulari/golangSoru2/main.go
+++ arka uç/arka uç-kodlama-sorulari/golangSoru2/main.go
@@ -4,4 +4,12 @@ package golangSoru2
 
 func SwapInt(i *int, i2 *int) {
 	// Kodu geliştiriniz
+	i = 33
+	i2 = 11
+
+	i = i + i2
+	i2 = i - i2
+	i = i - i2
+	fmt.Printf("Değişim Sonucu : i = %v, i2 = %v", i, i2)
+
 }
